fc8dd84c9f69f2cd05f540272a85a1d0ae6a9bea
Eduin Frank Garcia Cordero <eduinlight@gmail.com>
2024-11-17 16:25:05 +0100

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
+#include <cstdlib>
 #include <iostream>
 
-class A {
-public:
-  A(int a, int b) : a(a), b(b) {}
-  int a, b;
-};
-
 int main() {
-  A b(2,3);
-  for (int i = 0; i < 1000000; i++) {
-    std::cout << b.b << std::endl;
-  }
-
+  std::cout << "HI" << std::endl;
   return EXIT_SUCCESS;
 }
